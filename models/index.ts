6eb25b0230b4f011556be5f536cac3f8a0d1ce54
ilyass jmyi <ilyass@mobix.biz>
2025-02-06 11:24:09 +0000

    updates

---
+++ models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiErrorResponse';
+export * from './ApiFieldSchema';
+export * from './ApiModelSchema';
+export * from './ApiRelationshipSchema';
+export * from './QueryEntityWithRelations';
+export * from './QueryFilterResponse';
+export * from './QueryQueryFilter';
